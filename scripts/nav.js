4a056f337aa9fdb14f3138bcaccb9d3a24744f89
unstressed-dev <205306862+unstressed-dev@users.noreply.github.com>
2025-12-23 13:05:20 -0500

    sixty-ONE

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,8 @@
 const navbar = document.getElementById('main-nav');
 navbar.innerHTML = 
    `  <!--Logo Place Holder-->
-      <p class="logo">Devin.</p>
-      <ul class="navbar-links">
+      <p id="logo">Devin.</p>
+      <ul id="navbar-links">
          <li>
             <a href="#">Portfolio</a>
          </li>
