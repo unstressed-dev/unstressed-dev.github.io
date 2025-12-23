b65cab6899616fad276ab587a7bc5397c9c14384
unstressed-dev <205306862+unstressed-dev@users.noreply.github.com>
2025-12-22 21:38:27 -0500

    js yessir

---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById('main-nav');
+navbar.innerHTML = 
+   `  <!--Logo Place Holder-->
+      <p class="logo">Devin.</p>
+      <ul class="navbar-links">
+         <li>
+            <a href="#">Portfolio</a>
+         </li>
+      </ul>
+`;
